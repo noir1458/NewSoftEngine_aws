d0b2d019ff10853fcddec89707e2c93b5513555a
noir1458 <noir1458@gmail.com>
2024-06-09 03:05:05 +0900

    prod.py db

--- myproject/myproject/settings/prod.py
+++ myproject/myproject/settings/prod.py
@@ -1,3 +1,15 @@
 from .base import *
+import os
 
 ALLOWED_HOSTS = ['3.38.60.106']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'mydatabase'),
+        'USER': os.environ.get('DB_USER', 'myuser'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'mypassword'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
+    }
+}
