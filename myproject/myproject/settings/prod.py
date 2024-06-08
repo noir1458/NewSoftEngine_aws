bf043949a4004c5a2037c4f4e93d534510720cba
noir1458 <noir1458@gmail.com>
2024-06-09 04:22:38 +0900

    db

--- myproject/myproject/settings/prod.py
+++ myproject/myproject/settings/prod.py
@@ -1,20 +1,13 @@
 from .base import *
 import os
 
-DEBUG = True  # 일시적으로 True로 변경하여 디버깅
-
 ALLOWED_HOSTS = ['3.38.60.106']
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'mydatabase'),
-        'USER': os.environ.get('DB_USER', 'myuser'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'mypassword'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
